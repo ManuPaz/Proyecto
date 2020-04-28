f447b8110e995238733f3f5d5766fe13971a7c09
Manuel <Manuel@LAPTOP-R3PN53B6>
2020-04-28 14:32:54 +0200

    C

--- src/operaciones/Principal.java
+++ src/operaciones/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Arit ca = new Arit();
-		
+		System.out.println(ca.suma(5, 6));
+		System.out.println(ca.resta(7, 3));
 		System.out.println(ca.suma(5, 6));
 		System.out.println(ca.resta(7, 3));
 		ca.suma(5, 6);
